a80ec879a2a16fbe464db5e08d7ef746c4bf55cd
Salsabila Khroinsin <salsabilakhroinsin@gmail.com>
2020-04-30 12:49:36 +0700

    Initial commit

---
+++ PRG2_20192_P1_3_25/PRG2_20192_P1_3_25/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PRG2_20192_P1_3_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== PROGRAM MENULIS SYAIR ANAK AYAM ===");
+            Console.WriteLine("---------------------------------------");
+            //input Banyak anak Ayam
+            Console.Write("Banyak Anak Ayam (N) : ");
+            int N = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine(" ");
+            
+            int i;
+            for (i = 1; i <= N; N--)
+            {
+               if (N == 1)
+                {
+                    Console.WriteLine("Anak ayam turun {0}, mati satu tinggal induknya", N);
+                }
+                else
+                {
+                    Console.WriteLine("Anak ayam turun {0}, mati satu tinggal {1}", N, N-1);
+                }
+            }
+        }
+    }
+}
---
+++ PRG2_20192_P1_4_25/PRG2_20192_P1_4_25/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace PRG2_20192_P1_4_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== PRGRAM SEGITIGA SIKU-SIKU\n");
+            Console.WriteLine("MENU SEGITIGA SIKU-SIKU");
+            Console.WriteLine("a. Hitung Luas");
+            Console.WriteLine("b. Hitung Hipotenusa");
+            Console.WriteLine("c. Hitung Keliling");
+            Console.WriteLine("d. Keluar");
+
+            // input menu
+            Console.Write("Menu pilihan : ");
+            char menu = Convert.ToChar(Console.ReadLine());
+
+            double Luas = 0;
+            double simi = 0;
+            double Kel = 0;
+            switch (menu)
+            {
+                case 'a':
+                    Console.Write("Masukkan Alas Segitiga : ");
+                    double Alas = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Masukkan Tinggi Segitiga : ");
+                    double Tinggi = Convert.ToDouble(Console.ReadLine());
+                    Luas = Alas * Tinggi / 2;
+                    Console.WriteLine("Luas segitiga dengan alas {0} dan tinggi {1} adalah {2} satuan", Alas, Tinggi, Luas);
+                    break;
+
+                case 'b':
+                    Console.Write("Masukkan Alas Segitiga : ");
+                    Alas = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Masukkan Tinggi Segitiga : ");
+                    Tinggi = Convert.ToDouble(Console.ReadLine());
+                    simi = Math.Sqrt((Alas * Alas) + (Tinggi * Tinggi));
+                    Console.WriteLine("Sisi miring segitiga dengan alas {0} dan tinggi {1} adalah {2} satuan", Alas, Tinggi, simi);
+                    break;
+
+                case 'c':
+                    Console.Write("Masukkan Alas : ");
+                    Alas = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Masukkan Tinggi : ");
+                    Tinggi = Convert.ToDouble(Console.ReadLine());
+                    simi = Math.Sqrt((Alas * Alas) + (Tinggi * Tinggi));
+                    Kel = Alas + Tinggi + simi;
+                    Console.WriteLine("Keliling segitiga dengan alas {0}, tinggi {1}, dan sisi miring {2} adalah {3} satuan", Alas, Tinggi, simi, Kel);
+                    break;
+
+                case 'd':
+                    Console.Clear();
+                    Environment.Exit(0);
+                    break;
+            }   
+
+        }
+    }
+}
